26cbf46ef82abe8f6725753d2b98d37bd5594307
Gary Woodfine <gary.woodfine@threenine.co.uk>
2016-03-10 13:31:53 +0000

    refactoring

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Geek.I.Am  Website' });
+    res.render('index', { title: 'Geek.I.Am  Website' ,
+    content: 'This is some content coming from the controller',
+        footer: '&copy; Geek.I.Am  2016'
+    });
 });
 
 module.exports = router;
